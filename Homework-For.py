bf46b954b1082701a00d6b3e6046928a3481553f
KhusnutdinovRustem <167807173+KhusnutdinovRustem@users.noreply.github.com>
2024-05-07 13:43:10 +0300

    Add files via upload

--- Homework-For.py
+++ Homework-For.py
@@ -1,8 +1,6 @@
 car_list = ["BMW", "AUDI", "VOLVO", "FORD", "KIA"]
-for i in car_list:
-    print('Я езжу на автомобиле марки ' + i)
-
 cars_count = 0
-for i in range(len(car_list)):
+
+for i in car_list:
     cars_count += 10
-    print(cars_count)
+    print('Я езжу на автомобиле марки ' + i, cars_count)
